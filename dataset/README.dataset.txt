6ab51a4e48a15387cc9d28cbfa8889cdff4a6ceb
eng <eng02@connect.hku.hk>
2023-10-22 13:50:45 +0800

    trained with data
    first train

---
+++ dataset/README.dataset.txt
@@ -0,0 +1,6 @@
+# Mainbuilding > 2023-10-17 2:03pm
+https://universe.roboflow.com/mblabeling-yjg9s/mainbuilding
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- train/train.py
+++ train/train.py
@@ -4,8 +4,8 @@ model = YOLO("yolov8n.yaml")  # build a new model from scratch
 # model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
 # Use the model
-# model.train(data="coco128.yaml", epochs=3)  # train the model
-# metrics = model.val()  # evaluate model performance on the validation set
-# train_result = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
+model.train(data='C:\\Users\\enoch\\OneDrive\\桌面\\YOLO\\dataset\\data.yaml', epochs=100, patience=50)  # train the model
+metrics = model.val()  # evaluate model performance on the validation set
+train_result = model("C:\\Users\\enoch\\OneDrive\\桌面\\YOLO\\dataset\\train\\images\\IMG_0636_jpg.rf.5e35a83bc69a4a081ed09b48456f6384.jpg")  # predict on an image
 print("================================")
-path = model.export()  # export the model to .pt format
+# path = model.export()  # export the model to .pt format
