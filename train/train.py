2bc8aa6e0784b5c869d78c6c50255c23de5a1bde
eng <eng02@connect.hku.hk>
2024-01-07 23:20:55 +0800

    update train.py

--- train/train.py
+++ train/train.py
@@ -5,11 +5,8 @@ def main():
     model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
     # Use the model
-    model.train(data='C:\\Users\\enoch\\OneDrive\\桌面\\YOLO\\dataset\\data.yaml', epochs=300, patience=50, device=0, optimizer="Adam")  # train the model
+    model.train(data='C:\\Users\\enoch\\OneDrive\\桌面\\YOLO\\dataset\\data.yaml', epochs=300, patience=30, device=0, optimizer="Adam", val=False)  # train the model
     metrics = model.val()  # evaluate model performance on the validation set
-    print("****************************************************************")
-    train_result = model("C:\\Users\\enoch\\OneDrive\\桌面\\YOLO\\dataset\\train\\images\\img_23102023181455039223_jpg.rf.e78e7f1d2e672bde7f01ac5d8c6ec4f1.jpg")  # predict on an image
-    print("================================")
     # path = model.export()  # export the model to .pt format
 if __name__ == '__main__':
     main()
