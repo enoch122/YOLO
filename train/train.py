20affdf57ae8e4d60f7754a012c68fc849c0700b
eng <eng02@connect.hku.hk>
2024-01-07 20:02:50 +0800

    trained

--- train/train.py
+++ train/train.py
@@ -7,9 +7,6 @@ def main():
     # Use the model
     model.train(data='C:\\Users\\enoch\\OneDrive\\桌面\\YOLO\\dataset\\data.yaml', epochs=300, patience=50, device=0, optimizer="Adam")  # train the model
     metrics = model.val()  # evaluate model performance on the validation set
-    print("****************************************************************")
-    train_result = model("C:\\Users\\enoch\\OneDrive\\桌面\\YOLO\\dataset\\train\\images\\img_23102023181455039223_jpg.rf.e78e7f1d2e672bde7f01ac5d8c6ec4f1.jpg")  # predict on an image
-    print("================================")
     # path = model.export()  # export the model to .pt format
 if __name__ == '__main__':
     main()
