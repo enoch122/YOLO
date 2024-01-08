6a990ee9d536620234e8693d3c02fb363eaa9a53
eng <eng02@connect.hku.hk>
2024-01-08 10:13:17 +0800

    trained

--- train/train.py
+++ train/train.py
@@ -5,7 +5,7 @@ def main():
     model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
     # Use the model
-    model.train(data='C:\\Users\\enoch\\OneDrive\\桌面\\YOLO\\dataset\\data.yaml', epochs=300, patience=30, device=0, optimizer="Adam", val=False)  # train the model
+    model.train(data='C:\\Users\\enoch\\OneDrive\\桌面\\YOLO\\dataset\\data.yaml', epochs=300, patience=20, device=0, optimizer="RMSProp", val=False)  # train the model
     metrics = model.val()  # evaluate model performance on the validation set
     # path = model.export()  # export the model to .pt format
 if __name__ == '__main__':
